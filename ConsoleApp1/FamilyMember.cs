3380972f8cd2ba26cb6674addcabee8161bd9613
Elina <elina.eldarovna.spb@gmail.com>
2024-07-12 13:48:10 +0300

    qqq

---
+++ ConsoleApp1/FamilyMember.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GB_tasks
+{
+
+    /*Спроектируйте программу для построения генеалогического дерева.
+     * Учтите что у нас есть члены семьи у кого нет детей(дет). 
+     * Есть члены семьи у кого дети есть (взрослые). Есть мужчины и женщины.
+     */
+    public class FamilyMember
+    {
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public Gender Gender { get; set; }
+        public FamilyMember Father { get; set; }
+        public FamilyMember Mother { get; set; }
+        public FamilyMember Spouse { get; set; }
+        public FamilyMember[] GetGrandmothers()
+        {
+            return new FamilyMember[] { Mother.Mother, Father.Mother };
+        }
+        public FamilyMember[] GetGrandfathers()
+        {
+            return new FamilyMember[] { Mother.Father, Father.Father };
+        }
+    }
+
+    public enum Gender { man, woman }
+}
+
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,81 @@
+﻿using GB_tasks;
+
+namespace ConsoleApp1
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FamilyMember GrandmaOne = new FamilyMember()
+            {
+                Name = "Masha Malinina",
+                Age = 70,
+                Gender = Gender.woman
+            };
+            FamilyMember GrandpaOne = new FamilyMember()
+            {
+                Name = "Misha Malinin",
+                Age = 76,
+                Gender = Gender.man
+            };
+            FamilyMember GrandmaTwo = new FamilyMember()
+            {
+                Name = "Arina Kirova",
+                Age = 70,
+                Gender = Gender.woman
+            };
+            FamilyMember GrandpaTwo = new FamilyMember()
+            {
+                Name = "Oleg Kirov",
+                Age = 78,
+                Gender = Gender.man
+            };
+            FamilyMember MyMother = new FamilyMember()
+            {
+                Name = "Mila Kirova",
+                Age = 46,
+                Gender = Gender.woman,
+                Father = GrandpaOne,
+                Mother = GrandmaOne,
+            };
+
+            FamilyMember MyFather = new FamilyMember()
+            {
+                Name = "Nikita Kirov",
+                Age = 50,
+                Gender = Gender.man,
+                Father = GrandpaTwo,
+                Mother = GrandmaTwo
+                
+            };
+            FamilyMember Husband = new FamilyMember()
+            {
+                Name = "Alex Illen",
+                Age = 24,
+                Gender = Gender.man
+
+            };
+            FamilyMember Me= new FamilyMember()
+            {
+                Name = "Vasilisa Kirova",
+                Age = 20,
+                Gender = Gender.man,
+                Father = MyFather,
+                Mother = MyMother,
+                Spouse = Husband
+
+            };
+             var grandMothers = Me.GetGrandmothers();
+            var grandfathers = Me.GetGrandfathers();
+            Console.WriteLine(grandMothers[0].Name);
+            Console.WriteLine(grandMothers[1].Name);
+            Console.WriteLine();
+            Console.WriteLine(grandfathers[0].Name);
+            Console.WriteLine(grandfathers[1].Name);
+            Console.WriteLine();
+            Console.WriteLine(Me.Spouse.Name);
+            
+        }
+    }
+}
